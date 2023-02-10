519a8153519f1b095a9d97a6618ac9388b2785b0
Kimiaparand <120417079+Kimiaparand@users.noreply.github.com>
2023-02-10 15:52:31 +0330

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForcast(response) {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forcastElement = document.querySelector("#forcast");
   forcastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -47,11 +48,12 @@ function displayForcast(response) {
   forcastElement.innerHTML = forcastHTML;
 }
 
-function getForcast(coordinates) {
+function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "830dc77e57fb6a652a14c00f66be12a9";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForcast);
+  let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
+  let apiUrl1 = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl1).then(displayForecast);
 }
 
 function dispalyTemperature(response) {
@@ -77,7 +79,8 @@ function dispalyTemperature(response) {
     "alt",
     `https://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
-  getForcast(response.data.coord);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -121,4 +124,3 @@ fahrenheitLink.addEventListener("click", showfahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showcelsiusTemp);
 search("Moscow");
-displayForcast();
